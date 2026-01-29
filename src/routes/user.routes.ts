adb80a03b89b7ec91c6d1fefa5edea0b8608bf94
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-01-29 04:04:31 +0200

    db init migrations

--- src/routes/user.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import * as userController from '../controllers/user.controller'
-
-export const userRouter = Router()
-
-userRouter.get('/', userController.getUsers)
-userRouter.post('/', userController.createUser)
