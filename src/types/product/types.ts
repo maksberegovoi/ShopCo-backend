6462751427435113403defe80b4302005b45b13b
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-02-03 14:13:02 +0200

    remove unnecessary folders&files after dev merge

--- src/types/product/types.ts
+++
@@ -1,9 +0,0 @@
-export type ProductVariant = {
-    productId: number
-    sizeId: number
-    colorName: string
-    colorHex: string
-    stock: number
-}
-
-// export type ProductVariantSeed = Omit<ProductVariant, 'productId'>
