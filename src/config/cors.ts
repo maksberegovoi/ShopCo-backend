9ae5a48880a769e79a41358c5b138212274cb39b
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-02-10 20:39:51 +0200

    change cors urls validation

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,12 @@
 import { env, isDev } from '../env'
+import { z } from 'zod'
 
-const parseOrigins = (value: string) => value.split(',').map((o) => o.trim())
+const parseOrigins = (value: string): string[] =>
+    value.split(',').map((url) => {
+        const origin = url.trim()
+        z.url().parse(origin)
+        return origin
+    })
 
 export const allowedOrigins = parseOrigins(
     isDev ? env.CLIENT_URLS_DEV : env.CLIENT_URLS_PROD
